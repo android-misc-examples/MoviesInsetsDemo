0dbc2cfb8c9e6e6ca05789eb8e9b22ef4a9797c8
pgp <pgp@gmx.it>
2020-10-02 11:14:19 +0200

    move padding logic in separate class

--- app/src/main/java/com/github/razir/movies/MoviesActivity.kt
+++ app/src/main/java/com/github/razir/movies/MoviesActivity.kt
@@ -2,133 +2,43 @@ package com.github.razir.movies
 
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import android.hardware.display.DisplayManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.view.Surface
 import android.view.View
 import android.view.View.*
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import com.github.razir.movies.utils.GridItemDecorator
 import com.github.razir.movies.utils.getTestData
 import kotlinx.android.synthetic.main.activity_movies.*
-import android.view.Window
-import android.widget.Toast
-import androidx.core.view.*
 
 
 class MoviesActivity : AppCompatActivity() {
+
+    lateinit var paddingManager: PaddingManager
+    lateinit var viewList: List<View>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY)
         supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.argb(128, 0, 0, 0)))
 
-        if(hidden) supportActionBar?.hide()
+        paddingManager = PaddingManager(this)
+
+        if(PaddingManager.hidden) supportActionBar?.hide()
         setContentView(R.layout.activity_movies)
 
         initView()
+        viewList = listOf(moviesRecyclerView,hideActionBarButton)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             moviesRootLayout.systemUiVisibility = SYSTEM_UI_FLAG_LAYOUT_STABLE or
                     SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
         }
 
-        if(insetsPadding.isEmpty()) {
-            insetsPadding = mutableMapOf(
-                Surface.ROTATION_0 to
-                        mutableMapOf(
-                            "top" to pxFromDp(81f),
-                            "left" to 0,
-                            "bottom" to pxFromDp(48f),
-                            "right" to 0
-                        ),
-                Surface.ROTATION_90 to
-                        mutableMapOf(
-                            "top" to pxFromDp(81f),
-                            "left" to 0,
-                            "bottom" to 0,
-                            "right" to pxFromDp(48f)
-                        ),
-                Surface.ROTATION_180 to // not always called, have to enable setting first
-                        mutableMapOf(
-                            "top" to pxFromDp(81f),
-                            "left" to 0,
-                            "bottom" to pxFromDp(48f),
-                            "right" to 0
-                        ),
-                Surface.ROTATION_270 to
-                        mutableMapOf(
-                            "top" to pxFromDp(81f),
-                            "left" to pxFromDp(48f),
-                            "bottom" to 0,
-                            "right" to 0
-                        )
-
-            )
-
-            hiddenInsetsPadding = mutableMapOf(
-                Surface.ROTATION_0 to
-                        mutableMapOf(
-                            "top" to pxFromDp(24f),
-                            "left" to 0,
-                            "bottom" to pxFromDp(48f),
-                            "right" to 0
-                        ),
-                Surface.ROTATION_90 to
-                        mutableMapOf(
-                            "top" to pxFromDp(24f),
-                            "left" to 0,
-                            "bottom" to 0,
-                            "right" to pxFromDp(48f)
-                        ),
-                Surface.ROTATION_180 to // not always called, have to enable setting first
-                        mutableMapOf(
-                            "top" to pxFromDp(24f),
-                            "left" to 0,
-                            "bottom" to pxFromDp(48f),
-                            "right" to 0
-                        ),
-                Surface.ROTATION_270 to
-                        mutableMapOf(
-                            "top" to pxFromDp(24f),
-                            "left" to pxFromDp(48f),
-                            "bottom" to 0,
-                            "right" to 0
-                        )
-
-            )
-        }
-
-        if(dm == null) {
-            refreshPaddings()
-
-            val dl = object : DisplayManager.DisplayListener {
-                override fun onDisplayAdded(p0: Int) {
-
-                }
-
-                override fun onDisplayRemoved(p0: Int) {
-
-                }
-
-                override fun onDisplayChanged(p0: Int) {
-                    Toast.makeText(this@MoviesActivity, "Current orientation: "+windowManager.defaultDisplay.rotation, Toast.LENGTH_SHORT).show()
-                    refreshPaddings()
-                }
-            }
-
-            dm = getSystemService(DISPLAY_SERVICE) as DisplayManager
-            dm?.registerDisplayListener(dl,h)
-        }
-    }
-
-    fun refreshPaddings() {
-        hidden = !hidden
-        toggleActionBar(null)
+        paddingManager.registerDisplayListener(viewList)
     }
 
     private fun initView() {
@@ -139,44 +49,15 @@ class MoviesActivity : AppCompatActivity() {
         }
     }
 
-    private fun pxFromDp(dp: Float): Int {
-        return (dp * resources.displayMetrics.density).toInt()
-    }
+
 
     fun toggleActionBar(unused: View?) {
-        if(hidden)
+        if(PaddingManager.hidden)
             supportActionBar?.show()
         else
             supportActionBar?.hide()
-        hidden = !hidden
+        PaddingManager.hidden = !PaddingManager.hidden
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            val degrees = windowManager.defaultDisplay.rotation
-
-            listOf(hideActionBarButton, moviesRecyclerView).forEach {
-                if (hidden)
-                    it.updatePadding(
-                        top = hiddenInsetsPadding[degrees]!!["top"]!!,
-                        left = hiddenInsetsPadding[degrees]!!["left"]!!,
-                        bottom = hiddenInsetsPadding[degrees]!!["bottom"]!!,
-                        right = hiddenInsetsPadding[degrees]!!["right"]!!
-                    )
-                else
-                    it.updatePadding(
-                        top = insetsPadding[degrees]!!["top"]!!,
-                        left = insetsPadding[degrees]!!["left"]!!,
-                        bottom = insetsPadding[degrees]!!["bottom"]!!,
-                        right = insetsPadding[degrees]!!["right"]!!
-                    )
-            }
-        }
-    }
-
-    companion object {
-        var hidden = false
-        var insetsPadding : MutableMap<Int, MutableMap<String, Int>> = mutableMapOf()
-        var hiddenInsetsPadding : MutableMap<Int, MutableMap<String, Int>> = mutableMapOf()
-        val h = Handler()
-        var dm: DisplayManager? = null
+        paddingManager.adjustPaddings(viewList)
     }
 }
---
+++ app/src/main/java/com/github/razir/movies/PaddingManager.kt
@@ -0,0 +1,138 @@
+package com.github.razir.movies
+
+import android.hardware.display.DisplayManager
+import android.os.Build
+import android.os.Handler
+import android.view.Surface
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.updatePadding
+
+class PaddingManager(val activity: AppCompatActivity) {
+
+    private fun pxFromDp(dp: Float): Int {
+        return (dp * activity.resources.displayMetrics.density).toInt()
+    }
+
+    fun createMaps() {
+        if(insetsPadding.isEmpty()) {
+            insetsPadding = mutableMapOf(
+                Surface.ROTATION_0 to
+                        mutableMapOf(
+                            "top" to pxFromDp(81f),
+                            "left" to 0,
+                            "bottom" to pxFromDp(48f),
+                            "right" to 0
+                        ),
+                Surface.ROTATION_90 to
+                        mutableMapOf(
+                            "top" to pxFromDp(81f),
+                            "left" to 0,
+                            "bottom" to 0,
+                            "right" to pxFromDp(48f)
+                        ),
+                Surface.ROTATION_180 to // not always called, have to enable setting first
+                        mutableMapOf(
+                            "top" to pxFromDp(81f),
+                            "left" to 0,
+                            "bottom" to pxFromDp(48f),
+                            "right" to 0
+                        ),
+                Surface.ROTATION_270 to
+                        mutableMapOf(
+                            "top" to pxFromDp(81f),
+                            "left" to pxFromDp(48f),
+                            "bottom" to 0,
+                            "right" to 0
+                        )
+
+            )
+
+            hiddenInsetsPadding = mutableMapOf(
+                Surface.ROTATION_0 to
+                        mutableMapOf(
+                            "top" to pxFromDp(24f),
+                            "left" to 0,
+                            "bottom" to pxFromDp(48f),
+                            "right" to 0
+                        ),
+                Surface.ROTATION_90 to
+                        mutableMapOf(
+                            "top" to pxFromDp(24f),
+                            "left" to 0,
+                            "bottom" to 0,
+                            "right" to pxFromDp(48f)
+                        ),
+                Surface.ROTATION_180 to // not always called, have to enable setting first
+                        mutableMapOf(
+                            "top" to pxFromDp(24f),
+                            "left" to 0,
+                            "bottom" to pxFromDp(48f),
+                            "right" to 0
+                        ),
+                Surface.ROTATION_270 to
+                        mutableMapOf(
+                            "top" to pxFromDp(24f),
+                            "left" to pxFromDp(48f),
+                            "bottom" to 0,
+                            "right" to 0
+                        )
+
+            )
+        }
+    }
+
+    fun adjustPaddings(views: List<View>) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            createMaps()
+
+            val degrees = activity.windowManager.defaultDisplay.rotation
+
+            views.forEach {
+                if (hidden)
+                    it.updatePadding(
+                        top = hiddenInsetsPadding[degrees]!!["top"]!!,
+                        left = hiddenInsetsPadding[degrees]!!["left"]!!,
+                        bottom = hiddenInsetsPadding[degrees]!!["bottom"]!!,
+                        right = hiddenInsetsPadding[degrees]!!["right"]!!
+                    )
+                else
+                    it.updatePadding(
+                        top = insetsPadding[degrees]!!["top"]!!,
+                        left = insetsPadding[degrees]!!["left"]!!,
+                        bottom = insetsPadding[degrees]!!["bottom"]!!,
+                        right = insetsPadding[degrees]!!["right"]!!
+                    )
+            }
+        }
+    }
+
+    fun registerDisplayListener(views: List<View>) {
+        if(dm == null) {
+            adjustPaddings(views)
+
+            val dl = object : DisplayManager.DisplayListener {
+                override fun onDisplayAdded(p0: Int) {}
+
+                override fun onDisplayRemoved(p0: Int) {}
+
+                override fun onDisplayChanged(p0: Int) {
+                    Toast.makeText(activity, "Current orientation: "+activity.windowManager.defaultDisplay.rotation, Toast.LENGTH_SHORT).show()
+                    adjustPaddings(views)
+                }
+            }
+
+            dm = activity.getSystemService(AppCompatActivity.DISPLAY_SERVICE) as DisplayManager
+            dm?.registerDisplayListener(dl, h)
+        }
+    }
+
+    companion object {
+        var hidden = false
+        var insetsPadding : MutableMap<Int, MutableMap<String, Int>> = mutableMapOf()
+        var hiddenInsetsPadding : MutableMap<Int, MutableMap<String, Int>> = mutableMapOf()
+        val h = Handler()
+        var dm: DisplayManager? = null
+    }
+}
